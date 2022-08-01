2cec8f1035ae31665a04a25e5941d34c7b92b4eb
sasakiyudai <yudai14142@gmail.com>
2022-08-01 23:59:27 +0900

    add Banner in App & fix typo

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,23 @@ import React from "react";
 import "./App.css";
 import { requests } from "./request";
 import { Row } from "./components/Row";
+import { Banner } from "./components/Banner";
 
 function App() {
   return (
     <div className="App">
+      <Banner />
       <Row
-        title="NETFLIX ORIGUINALS"
-        fetchUrl={requests.feachNetflixOriginals}
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
         isLargeRow
       />
-      <Row title="Top Rated" fetchUrl={requests.feactTopRated} />
-      <Row title="Action Movies" fetchUrl={requests.feactActionMovies} />
-      <Row title="Comedy Movies" fetchUrl={requests.feactComedyMovies} />
-      <Row title="Horror Movies" fetchUrl={requests.feactHorrorMovies} />
-      <Row title="Romance Movies" fetchUrl={requests.feactRomanceMovies} />
-      <Row title="DOcumentaries" fetchUrl={requests.feactDocumentMovies} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentMovies} />
     </div>
   );
 }
