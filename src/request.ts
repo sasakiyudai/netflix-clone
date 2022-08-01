f02eb063cee912cfdbed5167139e12a8c02048e4
sasakiyudai <yudai14142@gmail.com>
2022-08-01 23:11:38 +0900

    hit api /discover/movie

--- src/request.ts
+++ src/request.ts
@@ -2,11 +2,11 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const requests = {
   feachTrending: `/trending/all/week?api_key=${API_KEY}&language=en-us`,
-  feachNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-  feactTopRated: `/discover/tv?api_key=${API_KEY}&languager=en-us`,
-  feactActionMovies: `/discover/tv?api_key=${API_KEY}&with_genres=28`,
-  feactComedyMovies: `/discover/tv?api_key=${API_KEY}&with_genres=35`,
-  feactHorrorMovies: `/discover/tv?api_key=${API_KEY}&with_genres=27`,
-  feactRomanceMovies: `/discover/tv?api_key=${API_KEY}&with_genres=10749`,
-  feactDocumentMovies: `/discover/tv?api_key=${API_KEY}&with_genres=99`,
+  feachNetflixOriginals: `/discover/movie?api_key=${API_KEY}&with_networks=213`,
+  feactTopRated: `/discover/movie?api_key=${API_KEY}&languager=en-us`,
+  feactActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  feactComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  feactHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  feactRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  feactDocumentMovies: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
 };
